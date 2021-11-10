e6b8c5500aa6b39e68b3826622fecb081bb14d89
Orlando Quispe <orlandodead16@gmail.com>
2021-11-09 23:35:48 -0400

    Segundo Parcial

---
+++ src/main/java/com/emergentes/modelo/evento.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.emergentes.modelo;
+
+/**
+ *
+ * @author MiPC
+ */
+public class evento {
+    private int id;
+private String titulo;
+private String expositor;
+private String fecha;
+private String hora;
+private int cupo;
+
+public evento() {
+        this.id=0;
+        this.titulo="";
+        this.expositor="";
+        this.fecha="";
+        this.hora="";
+        this.cupo=0;
+    }
+public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getExpositor() {
+        return expositor;
+    }
+
+    public void setExpositor(String expositor) {
+        this.expositor = expositor;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public int getCupo() {
+        return cupo;
+    }
+
+    public void setCupo(int cupo) {
+        this.cupo = cupo;
+    }
+
+    @Override
+    public String toString() {
+        return "seminarios{" + "id=" + id + ", titulo=" + titulo + ", expositor=" + expositor +", fecha=" + fecha + ", hora=" + hora +", cupo=" + cupo + '}';
+    }
+}
